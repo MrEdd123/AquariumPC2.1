5051983d2aa647b9b8e05a523029966162768ab9
Andre Reichl <andre.reichl@gmx.de>
2019-11-15 18:31:28 +0100

    Projektdateien hinzufügen.

---
+++ AquariumPC2.1.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		AquariumPC2.ino
+ Created:	15.11.2019 18:25:17
+ Author:	andre
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
